13749b1632d13429c9eaf3fa66dd551c94b004d3
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-04-02 23:36:04 +0530

    Routing completed

---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,9 @@
+const Meeting = ({ params } : { params : { id: string }}) => {
+    return (
+        <div>
+            <h1>Meeting Room: #{params?.id}</h1>
+        </div>
+    );
+};
+
+export default Meeting;
