020d365256f0a2fae0d99549061c9662bcb16192
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-04-07 22:23:25 +0530

    Update

--- providers/streamClientProvider.tsx
+++ providers/streamClientProvider.tsx
@@ -1,38 +1,38 @@
-'use client';
-
-import { ReactNode, useEffect, useState } from 'react';
-import { StreamVideoClient, StreamVideo } from '@stream-io/video-react-sdk';
-import { useUser } from '@clerk/nextjs';
-
+"use client";
 import { tokenProvider } from '@/actions/stream.actions';
 import Loader from '@/components/Loader';
+import { useUser } from '@clerk/nextjs';
+import { StreamVideo, StreamVideoClient } from '@stream-io/video-react-sdk';
+import { ReactNode, useEffect, useState } from 'react';
 
-const API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
 const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+
   const [videoClient, setVideoClient] = useState<StreamVideoClient>();
   const { user, isLoaded } = useUser();
 
   useEffect(() => {
     if (!isLoaded || !user) return;
-    if (!API_KEY) throw new Error('Stream API key is missing');
-
+    if (!apiKey) throw new Error('Stream API key is not provided');
     const client = new StreamVideoClient({
-      apiKey: API_KEY,
-      user: {
+      apiKey, user: {
         id: user?.id,
         name: user?.username || user?.id,
-        image: user?.imageUrl,
+        image: user?.imageUrl
       },
       tokenProvider,
-    });
-
+    })
     setVideoClient(client);
   }, [user, isLoaded]);
 
-  if (!videoClient) return <Loader />;
+  if(!videoClient) return <Loader />
 
-  return <StreamVideo client={videoClient}>{children}</StreamVideo>;
+  return (
+    <StreamVideo client={videoClient}>
+      {children}
+    </StreamVideo>
+  );
 };
 
 export default StreamVideoProvider;
