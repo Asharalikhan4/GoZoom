61a423f452b489e90d9ea828b0544161b7f72635
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-04-07 12:57:19 +0530

    Error resolving

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,24 @@
-import { authMiddleware } from '@clerk/nextjs/server';
+import { authMiddleware, AuthMiddlewareParams } from '@clerk/nextjs/server';
+import { NextApiRequest } from 'next';
 
-const protectedRoutes = {
-    matcher: ([
-        '/',
-        '/upcoming',
-        '/meeting(.*)',
-        '/previous',
-        '/recordings',
-        '/personal-room',
-    ])
-};
+const protectedRoutes = [
+    '/',
+    '/upcoming',
+    '/meeting(.*)',
+    '/previous',
+    '/recordings',
+    '/personal-room',
+];
 
-export default authMiddleware((auth, req) => {
-  if (protectedRoutes(req)) auth().protect();
+export default authMiddleware((auth: AuthMiddlewareParams | undefined, req: NextApiRequest) => {
+    if (auth) {
+        const { protect } = auth();
+        if (protectedRoutes.some(route => req.url?.startsWith(route))) {
+            protect();
+        }
+    }
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
